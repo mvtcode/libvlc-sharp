f0e47bb29b0f3c839a77c6c61ba039e3ba65ea25
tjfontaine <tjfontaine@a9c47ebf-7b2b-0410-a00a-61212fad1899>
2007-11-27 20:45:10 +0000

    import scotts work

---
+++ branches/libvlc-sharp-scott/VlcInput.cs
@@ -0,0 +1,383 @@
+/* vi:set ts=4 et sts=4 sw=4: */
+/***************************************************************************
+ *  VlcInput.cs
+ *  Original Filename: VlcInput.cs
+ * 
+ *  Copyright (C) 2007 Timothy J Fontaine <tjfontaine@gmail.com>
+ *  Written by Timothy J Fontaine <tjfontaine@gmail.com>
+ *  Modified by Scott E Graves <scott.e.graves@gmail.com>
+ ****************************************************************************/
+
+/*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),  
+ *  to deal in the Software without restriction, including without limitation  
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+ *  and/or sell copies of the Software, and to permit persons to whom the  
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace libvlc
+{
+    public class VlcInputHandle : SafeHandle
+    {
+        private bool disposed = false;
+
+        public VlcInputHandle() : base(IntPtr.Zero, true)
+        { }
+
+        public override bool IsInvalid
+        {
+            get { return handle == IntPtr.Zero; }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            if (!IsInvalid && !disposed)
+            {
+                disposed = true;
+                libvlc_input_free(this);
+            }
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                ReleaseHandle();
+            }
+            base.Dispose(disposing);
+        }
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_input_free(VlcInputHandle input);
+    }
+
+    public class VlcInput : IDisposable
+    {
+        private bool _disposed = false;
+        private VlcInputHandle _input;
+        private VlcException _excp = new VlcException();
+
+        internal VlcInput(VlcInstanceHandle _inst)
+        {
+            _input = libvlc_playlist_get_input(_inst, _excp);
+        }
+
+        ~VlcInput()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                if (!_input.IsInvalid)
+                    _input.Dispose();
+            }
+        }
+
+        private void handle_exception()
+        {
+            if (_excp.Raised)
+            {
+                VlcInstanceException vlcex;
+                switch (_excp.Message)
+                {
+                    case "No active input":
+                        vlcex = new VlcNoActiveInputException(_excp.Message) as VlcInstanceException;
+                        break;
+                    default:
+                        vlcex = new VlcInstanceException(_excp.Message);
+                        break;
+                }
+                throw vlcex;
+            }
+        }
+
+        internal IntPtr Owner
+        {
+            set
+            {
+                libvlc_video_reparent(_input, value, _excp);
+                handle_exception();
+            }
+        }
+
+        public bool IsInvalid
+        {
+            get
+            {
+                return _input.IsInvalid;
+            }
+        }
+
+        public Int64 Time
+        {
+            get
+            {
+                Int64 ret = libvlc_input_get_time(_input, _excp);
+                handle_exception();
+                return ret;
+            }
+
+            set
+            {
+                libvlc_input_set_time(_input, value, _excp);
+                handle_exception();
+            }
+        }
+
+        public float Position
+        {
+            get
+            {
+                float ret = libvlc_input_get_position(_input, _excp);
+                handle_exception();
+                return ret;
+            }
+            set
+            {
+                libvlc_input_set_position(_input, value, _excp);
+                handle_exception();
+            }
+        }
+
+        public Int64 Length
+        {
+            get
+            {
+                Int64 ret = libvlc_input_get_length(_input, _excp);
+                handle_exception();
+                return ret;
+            }
+        }
+
+        public float Rate
+        {
+            get
+            {
+                float i = libvlc_input_get_rate(_input, _excp);
+                handle_exception();
+                return i;
+            }
+
+            set
+            {
+                libvlc_input_set_rate(_input, value, _excp);
+                handle_exception();
+            }
+        }
+
+
+        public bool WillPlay
+        {
+            get
+            {
+                bool b = libvlc_input_will_play(_input, _excp);
+                handle_exception();
+                return b;
+            }
+        }
+
+
+        public Int32 State
+        {
+            get
+            {
+                Int32 i = libvlc_input_get_state(_input, _excp);
+                handle_exception();
+                return i;
+            }
+        }
+
+
+        public bool HasVout
+        {
+            get
+            {
+                bool b = libvlc_input_has_vout(_input, _excp);
+                handle_exception();
+                return b;
+            }
+        }
+
+
+        public float FPS
+        {
+            get
+            {
+                float i = libvlc_input_get_fps(_input, _excp);
+                handle_exception();
+                return i;
+            }
+        }
+
+
+        public bool VideoFullScreen
+        {
+            get
+            {
+                bool b = libvlc_get_fullscreen(_input, _excp);
+                handle_exception();
+                return b;
+            }
+			
+			set
+			{
+				libvlc_set_fullscreen(_input, value, _excp);
+				handle_exception();
+			}
+        }
+
+
+        public Int32 VideoHeight
+        {
+            get
+            {
+                Int32 i = libvlc_video_get_height(_input, _excp);
+                handle_exception();
+                return i;
+            }
+        }
+
+
+        public Int32 VideoWidth
+        {
+            get
+            {
+                Int32 i = libvlc_video_get_width(_input, _excp);
+                handle_exception();
+                return i;
+            }
+        }
+
+
+        public string VideoAspectRatio
+        {
+            get
+            {
+                string s = libvlc_video_get_aspect_ratio(_input, _excp);
+                handle_exception();
+                return s;
+            }
+
+            set
+            {
+                libvlc_video_set_aspect_ratio(_input, value, _excp);
+                handle_exception();
+            }
+        }
+
+
+        public void VideoToggleFullScreen()
+        {
+            libvlc_toggle_fullscreen(_input, _excp);
+            handle_exception();
+        }
+
+
+        public void TakeSnapshot(string fileName)
+        {
+            libvlc_video_take_snapshot(_input, fileName, _excp);
+            handle_exception();
+        }
+
+        #region libvlc api
+        [DllImport("libvlc")]
+        private static extern VlcInputHandle libvlc_playlist_get_input(VlcInstanceHandle engine, VlcExceptionHandle ex);
+
+        [DllImport("libvlc")]
+        private static extern int libvlc_video_reparent(VlcInputHandle p_input, IntPtr hwnd, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern Int64 libvlc_input_get_length(VlcInputHandle input, VlcExceptionHandle exception);
+
+        [DllImport("libvlc")]
+        private static extern Int64 libvlc_input_get_time(VlcInputHandle input, VlcExceptionHandle exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_input_set_time(VlcInputHandle input, Int64 time, VlcExceptionHandle exception);
+
+        [DllImport("libvlc")]
+        private static extern float libvlc_input_get_position(VlcInputHandle input, VlcExceptionHandle exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_input_set_position(VlcInputHandle input, float pos, VlcExceptionHandle exception);
+
+        [DllImport("libvlc")]
+        private static extern float libvlc_input_get_rate(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_input_set_rate(VlcInputHandle p_input, float f, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern Int32 libvlc_input_get_state(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern bool libvlc_input_will_play(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern bool libvlc_input_has_vout(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern float libvlc_input_get_fps(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_set_fullscreen(VlcInputHandle p_input, bool b, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern bool libvlc_get_fullscreen(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern int libvlc_video_get_height(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern int libvlc_video_get_width(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern string libvlc_video_get_aspect_ratio(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_video_set_aspect_ratio(
+            VlcInputHandle p_input,
+            [MarshalAs(UnmanagedType.LPStr)] string aspect_ratio,
+            VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_toggle_fullscreen(VlcInputHandle p_input, VlcExceptionHandle p_exception);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_video_take_snapshot(
+            VlcInputHandle p_input,
+            [MarshalAs(UnmanagedType.LPStr)] 
+            string file_name,
+            VlcExceptionHandle p_exception);
+        #endregion
+
+    }
+}
---
+++ branches/libvlc-sharp-scott/VlcLogEnum.cs
@@ -0,0 +1,193 @@
+/* vi:set ts=4 et sts=4 sw=4: */
+/***************************************************************************
+ *  VlcLogEnum.cs
+ *  Original Filename: VlcLogEnum.cs
+ *
+ *  Copyright (C) 2007 Timothy J Fontaine <tjfontaine@gmail.com>
+ *  Written by Timothy J Fontaine <tjfontaine@gmail.com>
+ *  Modified by Scott E Graves <scott.e.graves@gmail.com>
+ ****************************************************************************/
+
+/*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),  
+ *  to deal in the Software without restriction, including without limitation  
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+ *  and/or sell copies of the Software, and to permit persons to whom the  
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace libvlc
+{
+    public class VlcLogEnumException : Exception
+    {
+        public VlcLogEnumException(string m) : base(m) {}
+    }
+
+    public class VlcLogEnumHandle : SafeHandle
+    {
+        public VlcLogEnumHandle() : base(IntPtr.Zero, true)
+        { }
+
+        public override bool IsInvalid
+        {
+            get { return handle == IntPtr.Zero; }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            if(!IsInvalid)
+            {
+                VlcException ex = new VlcException();
+                libvlc_log_iterator_free(this, ex);
+                if(!ex.Raised)
+                {
+                    handle = IntPtr.Zero;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Failed to Release VlcLogEnum Handle: " + ex.Message);
+                    ex.Clear();
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            ReleaseHandle();
+            base.Dispose(disposing);
+        }
+        
+        [DllImport("libvlc")]
+        private static extern void libvlc_log_iterator_free(VlcLogEnumHandle iter, VlcExceptionHandle ex);
+    }
+
+    public class VlcLogEnum : IEnumerator
+    {
+        private VlcLogHandle log;
+        private VlcLogEnumHandle iter;
+        private VlcLogMessageHandle last_ptr;
+
+        public VlcLogEnum(VlcLogHandle log)
+        {
+            this.log = log;
+            get_iter();
+        }
+
+        private void get_iter() { VlcException ex = new VlcException(); get_iter(ex); }
+
+        private void get_iter(VlcException ex)
+        {
+            iter = libvlc_log_get_iterator(log, ex);
+            handle_exception(ex);
+        }
+
+        private void free(VlcException ex)
+        {
+            if(!iter.IsInvalid)
+            {
+                libvlc_log_iterator_free(iter, ex);
+                handle_exception(ex);
+            }
+        }
+
+        private bool has_next(VlcException ex)
+        {
+            int ret = libvlc_log_iterator_has_next(iter, ex);
+            handle_exception(ex);
+            return ret == 1 ? true : false;
+        }
+
+        private void handle_exception(VlcException ex)
+        {
+            if(ex.Raised)
+            {
+                VlcLogEnumException vlee = new VlcLogEnumException(ex.Message);
+                Console.WriteLine(vlee);
+                ex.Clear();
+                throw vlee;
+            }
+        }
+
+        public virtual bool MoveNext()
+        {
+            VlcException ex = new VlcException();
+            if(has_next(ex))
+            {
+                int size = Marshal.SizeOf(typeof(VlcLogMessagePtr));
+
+                //libvlc wants us to allocate the buffer and set the size
+                //so it knows that the structs match in layout and size on
+                //runtime arch
+                VlcLogMessagePtr tmp = new VlcLogMessagePtr();
+                tmp.message_size = (uint)size;
+
+                //take our existing structure and put it in the unmanaged memory
+                //also let it deallocate the original
+                VlcLogMessageHandle ptr = new VlcLogMessageHandle();
+                Marshal.StructureToPtr(tmp, ptr, true);
+
+                last_ptr = libvlc_log_iterator_next(iter, ptr, ex);
+
+                if(!ex.Raised)
+                    return true;
+            
+                handle_exception(ex);
+                return false;
+            }
+            return false;
+        }
+
+        public virtual void Reset()
+        {
+            VlcException ex = new VlcException();
+            free(ex);
+            get_iter(ex);
+        }
+
+        public virtual Object Current
+        {
+            get
+            {
+                if(!last_ptr.IsInvalid)
+                {
+                    VlcLogMessage message = new VlcLogMessage(last_ptr);
+                    return message;
+                }
+                return null;
+            }
+        }
+
+        [DllImport("libvlc")]
+        private static extern VlcLogEnumHandle libvlc_log_get_iterator(VlcLogHandle log, VlcExceptionHandle ex);
+
+        [DllImport("libvlc")]
+        private static extern void libvlc_log_iterator_free(VlcLogEnumHandle iter, VlcExceptionHandle ex);
+
+        [DllImport("libvlc")]
+        private static extern int libvlc_log_iterator_has_next(VlcLogEnumHandle iter, VlcExceptionHandle ex);
+
+        [DllImport("libvlc")]
+        private static extern VlcLogMessageHandle libvlc_log_iterator_next(VlcLogEnumHandle iter, VlcLogMessageHandle buffer, VlcExceptionHandle ex);
+    }
+}
